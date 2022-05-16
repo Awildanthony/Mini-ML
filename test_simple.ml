cb38ec1d2edcef915a1adef7f344de32bb6bbb10
Anthony Malysz <90163581+Awildanthony@users.noreply.github.com>
2022-05-15 21:37:57 -0400

    Update test_simple.ml

--- test_simple.ml
+++ test_simple.ml
@@ -1,86 +1,21 @@
-open Miniml ;;
+(*
+              CS 51 Really Simple Unit Testing Framework
+ *)                          
 
-(* ...........{ UNIVERSAL SAMPLE EXPRESSIONS }.......... *)
-let s_to_e = str_to_exp ;;
-                                                                   (* var(s) *)
-let var_x = s_to_e "x;;" ;; 
-                                                                     (* nums *)
-let int_pos = s_to_e "42;;" ;; 
-let int_neg = s_to_e "~-42;;" ;;
-                                                                   (* floats *)
-let flo_pos = s_to_e "42.;;" ;; 
-let flo_neg = s_to_e "~-42.;;" ;;
-                                                                    (* bools *)
-let bool_t = s_to_e "true;;" ;; 
-let bool_f = s_to_e "false;;" ;;
-                                                                    (* unops *)
-let neg_var = s_to_e "~-x;;" ;; 
-let not_var = s_to_e "not x;;" ;;
-                                                           (* binops: --nums *)
-let bin_add_i = s_to_e "x + 42;;" ;; 
-let bin_sub_i = s_to_e "x - 42;;" ;;     
-let bin_mul_i = s_to_e "x * 42;;" ;;
-let bin_div_i = s_to_e "x / 41;;" ;; 
-let bin_pow_i = s_to_e "x ** 3.;;" ;; 
-let bin_equ_i = s_to_e "x = 42;;" ;;
-let bin_les_i = s_to_e "x < 42;;" ;;
-let bin_leq_i = s_to_e "x <= 42;;" ;;
-let bin_gre_i = s_to_e "x > 42;;" ;;
-let bin_geq_i = s_to_e "x >= 42;;" ;;
-let bin_neq_i = s_to_e "x <> 42;;" ;;
-                                                         (* binops: --floats *)
-let bin_add_f = s_to_e "x +. 42.;;" ;;   
-let bin_sub_f = s_to_e "x -. 42.;;" ;;
-let bin_mul_f = s_to_e "x *. 42.;;" ;;
-let bin_div_f = s_to_e "x /. 41.;;" ;; 
-let bin_pow_f = s_to_e "x ** 3.;;" ;; 
-let bin_equ_f = s_to_e "x = 42.;;" ;;
-let bin_les_f = s_to_e "x < 42.;;" ;;
-let bin_leq_f = s_to_e "x <= 42.;;" ;;
-let bin_gre_f = s_to_e "x > 42.;;" ;;
-let bin_geq_f = s_to_e "x >= 42.;;" ;;
-let bin_neq_f = s_to_e "x <> 42.;;" ;;
-                                                             (* conditionals *)
-let cond_0 = s_to_e "if x = 42 then true else y;;" ;; 
-let cond_1 = s_to_e "if x = 42. then true else y;;" ;;
-                                                                      (* fun *)
-let fun0 = s_to_e "fun x -> x + x ;;" ;; 
-let fun0' = s_to_e "fun x -> x +. x ;;"
-let fun1 = s_to_e "fun y -> f (x + y);;" ;;
-let fun1' = s_to_e "fun y -> f (x +. y);;" ;;
-let fun2 = s_to_e "fun y -> x + y;;" ;;
-let fun2' = s_to_e "fun y -> x +. y;;" ;;
-                                                                   (* let... *)
-let leT0 = s_to_e "let x = 42 in x + x;;" ;; 
-let leT0' = s_to_e "let x = 42. in x +. x;;" ;; 
-let leT1 = s_to_e "let x = 42 in x + y;;" ;;
-let leT1' = s_to_e "let x = 42. in x +. y;;" ;;
-                                                               (* let rec... *)
-let leTR = s_to_e "let rec x = fun y -> x in x;;" ;; 
-                                                                      (* app *)
-let ap0 = s_to_e "let sample = fun x -> 42 * x in sample 42;;" ;; 
-let ap0' = s_to_e "let sample = fun x -> 42. *. x in sample 42.;;" ;;
-let ap1 = s_to_e "f (x + y);;" ;;
-let ap1' = s_to_e "f (x +. y);;" ;;
-
-
-(* counter reset each time ./tests.byte is called *)
-let ctr = ref 0 ;;
-
-(* adapted from the original unit_test by CS51 staff, but this time
-   disregarding the need for a msg and instead printing the test # *)
-let unit_test (condition : bool) : unit =  
- let count : unit -> int = 
-    fun () ->
-        ctr := !ctr + 1; 
-        !ctr  in 
+(* unit_test test msg -- Returns unit, with side effect of printing a
+   report identified by msg on whether the unit test passed (returned
+   true) or failed (returned false) *)
+let unit_test (condition : bool) (msg : string) : unit =  
   if condition then
-    (Printf.printf "test %i " (count ()) ;
-    Printf.printf "passed\n")
+    Printf.printf "%s passed\n" msg
   else
-    (Printf.printf "test %i " (count ()) ;
-    Printf.printf "FAILED\n") ;;
+    Printf.printf "%s FAILED\n" msg ;;
 
-
-
-let ut x = unit_test x ;; (* shorthand *)
+(* unit_test_within tolerance value1 value2 msg -- Tests that value1
+   is within tolerance of value2. Identifies test using msg. *)
+let unit_test_within (tolerance : float)
+                     (value1 : float)
+                     (value2 : float)
+                     (msg : string)
+                   : unit =
+  unit_test (abs_float (value1 -. value2) < tolerance) msg ;;
